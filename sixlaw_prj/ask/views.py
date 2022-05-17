217b5824137cb5c9198a578a5779b49c7e8a5bbd
Hyo <kyh12180@gmail.com>
2022-05-17 20:05:40 +0900

    ask와 store 연결

--- sixlaw_prj/ask/views.py
+++ sixlaw_prj/ask/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+
+class PostList(ListView) :
+    model = Post
+    ordering = '-pk'
+    template_name = 'ask/ask_list.html'
+
+    def get_context_data(self, **kwargs) :
+        context = super(PostList, self).get_context_data()
+        return context
+
+class PostDetail(DetailView) :
+    model = Post
+
+    def get_context_data(self, **kwargs) :
+        context = super(PostDetail, self).get_context_data()
+        return context
--- sixlaw_prj/store/models.py
+++ sixlaw_prj/store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model) : 
+    title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    # pk : 기본키
+    def __str__(self) :
+        return f'[{self.pk}] {self.title}::{self.author}'
+
+    def get_absolute_url(self) :
+        return f'/store/{self.pk}/'
