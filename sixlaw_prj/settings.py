33d98fe0219e9e3918067f9cb637ee8321e5fb5e
Hyo <kyh12180@gmail.com>
2022-05-26 00:36:37 +0900

    기본 스토어 기능 추가 완료, 좌석 표시와 평균 평점 기능 남음.

--- sixlaw_prj/settings.py
+++ sixlaw_prj/settings.py
@@ -142,3 +142,9 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.StoreList.as_view()), 
     path('<int:pk>/', views.StoreDetail.as_view()),
     path('<int:pk>/new_review/', views.new_Review),
+    path('update_store/<int:pk>/', views.StoreUpdate.as_view()),
 ]
