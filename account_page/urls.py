8418632c949076d3ffa33ce6360b638b2f3a527e
Hyo <kyh12180@gmail.com>
2022-05-27 21:58:32 +0900

    로그인 회원가입 로그아웃 추가

--- account_page/urls.py
+++ account_page/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.account), 
-    
+    path('logout/', views.logout), 
+    path('login/', views.login_page), 
+    path('signup/', views.signup_page),  
+    path('do_login/', views.login, name='login'),
+    path('do_signup/', views.signup, name='signup'),
 ]
--- account_page/views.py
+++ account_page/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import User
+from django.contrib import auth
 
 # Create your views here.
-def account(request) :
-
+def login_page(request) :
     return render (
         request, 
-        'account_page/account.html', 
+        'account_page/login.html', 
     )
+
+def signup_page(request) :
+    return render (
+        request, 
+        'account_page/signup.html', 
+    )
+
+def login(request) :
+    if request.method == "POST" :
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=email, password=password)
+        if user is not None :
+            auth.login(request, user)
+            return redirect('/store')
+        else :
+            return render(request, 'login.html', {'error' : 'e-mail or password is incorrect.'})
+    else :
+        return render(request, 'login.html')
+
+def signup(request) :
+    if request.method == "POST" :
+        if request.POST["password"] == request.POST['check_password'] :
+            user = User.objects.create_user(
+                email = request.POST['email'],
+                nickname = request.POST['nickname'],
+                password = request.POST['password'],
+                date_of_birth = request.POST['date_of_birth'],
+                is_seller = request.POST['is_seller'],
+            )
+            auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('/store')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
+
+def logout(request) :
+    auth.logout(request)
+    return redirect('/')
