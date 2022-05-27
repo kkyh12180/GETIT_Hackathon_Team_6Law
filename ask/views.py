c79139bce20588bd4cb3d3aed129b92090322ca4
Karakuritamashi <harry3697@naver.com>
2022-05-27 15:16:37 +0900

    ask기능(답변제외)

--- ask/views.py
+++ ask/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post
+
+from ensurepip import bootstrap
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Post, Category
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+
 
 # Create your views here.
 
@@ -19,3 +24,51 @@ class PostDetail(DetailView) :
     def get_context_data(self, **kwargs) :
         context = super(PostDetail, self).get_context_data()
         return context
+
+
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'file_upload','category']
+    template_name = 'ask/ask_form.html'
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/ask/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content', 'file_upload', 'category']
+    template_name = 'ask/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+def category_page(request, slug):
+    if slug == 'no category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'ask/ask_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )    
