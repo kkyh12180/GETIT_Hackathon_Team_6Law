bd56a0ff8ed23b1108da873027e64023fb8abaee
Hyo <kyh12180@gmail.com>
2022-05-27 23:58:34 +0900

    가이드 완성, footer 하단 고정

--- guide/views.py
+++ guide/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post
 
 # Create your views here.
 
-class PostList(ListView) :
-    model = Post
-    ordering = '-pk'
-    template_name = 'guide/guide_list.html'
-
-    def get_context_data(self, **kwargs) :
-        context = super(PostList, self).get_context_data()
-        return context
-
-class PostDetail(DetailView) :
-    model = Post
-
-    def get_context_data(self, **kwargs) :
-        context = super(PostDetail, self).get_context_data()
-        return context
+def show_guide(request) :
+    return render (
+        request, 
+        'guide/guide.html', 
+    )
